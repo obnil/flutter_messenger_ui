db056efe909438fcdf78dd8355842069d1adfeea
lb891014 <lb891014@126.com>
2020-01-29 18:08:58 +0800

    flutter chat app first blood

---
+++ README.md
@@ -0,0 +1,24 @@
+# flutter_messenger_app
+
+A new Flutter project.
+
+Design:https://dribbble.com/shots/6428387-Messenger-Mobile-Concept
+
+<figure class="half">
+<img src="./screenshots/destination_screen.jpg" alt="destination_screen" style="zoom:25%;" />
+<img src="./screenshots/home_screen.jpg" alt="home_screen" style="zoom:25%;" />
+</figure>
+
+
+## Getting Started
+
+This project is a starting point for a Flutter application.
+
+A few resources to get you started if this is your first Flutter project:
+
+- [Lab: Write your first Flutter app](https://flutter.dev/docs/get-started/codelab)
+- [Cookbook: Useful Flutter samples](https://flutter.dev/docs/cookbook)
+
+For help getting started with Flutter, view our
+[online documentation](https://flutter.dev/docs), which offers tutorials,
+samples, guidance on mobile development, and a full API reference.
---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_messenger_app/screens/favorite_contacts.dart';
+import 'package:flutter_messenger_app/screens/message_chats.dart';
+import 'package:flutter_messenger_app/widgets/category_selector.dart';
+
+class HomeScreen extends StatefulWidget {
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Theme.of(context).primaryColor,
+      appBar: AppBar(
+        leading: IconButton(
+          icon: Icon(Icons.menu),
+          iconSize: 30.0,
+          color: Colors.white,
+          onPressed: () {},
+        ),
+        title: Text('Chats',
+            style: TextStyle(
+              fontSize: 28.0,
+              fontWeight: FontWeight.bold,
+            )),
+        elevation: 0.0,
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.search),
+            iconSize: 30.0,
+            color: Colors.white,
+            onPressed: () {},
+          ),
+        ],
+      ),
+      body: Column(
+        children: <Widget>[
+          CategorySelector(),
+          Expanded(
+            child: Container(
+              height: 500.0,
+              decoration: BoxDecoration(
+                  color: Theme.of(context).accentColor,
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(30.0),
+                    topRight: Radius.circular(30.0),
+                  )),
+              child: Column(
+                children: <Widget>[
+                  FavoriteContacts(),
+                  MessageChats(),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
